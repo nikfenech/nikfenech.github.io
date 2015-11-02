a11b20774dfed47af23da57a10a2516effcea52f
nikfenech <nik.fenech@gmail.com>
2015-11-02 22:18:43 +1100

    Create day21.css

---
+++ day21.css
@@ -0,0 +1,50 @@
+*{
+	margin: 0;
+	padding: 0;
+}
+
+body{
+	background-color: #42A5F5;
+	font-family: arial, sans-serif;
+	color: #FFFFFF;
+}
+
+.ultimo{
+	width: 750px;
+	height: 220px;
+	margin: 50px auto 0 auto;
+	background-color: #BBDEFB;
+	border-radius: 10px;
+	padding: 20px 0 0 20px;
+}
+
+.vote{
+	width: 30%;
+}
+
+.updown{
+	width: 25px;
+	float: left;
+	margin: 0 100px 0 0;
+	padding: 0 0 0 50px;
+}
+
+.textbody{
+	width: 550px;
+	float: left;
+}
+
+#voteNo{
+	width: 50px;
+	float: left;
+	margin: 0 0 0 -10px;
+}
+
+h1{
+	font-size: 3em;
+	float: left;
+}
+
+p{
+	padding: 5px 0 0 0;
+}
