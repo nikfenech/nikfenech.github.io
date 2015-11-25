fc4bb03aaab5f1fce51166aac5c571fd3187f62a
nikfenech <nik.fenech@gmail.com>
2015-11-25 14:54:45 +1100

    Update day28.js

--- day28.js
+++ day28.js
@@ -1,4 +1,4 @@
-var eye=["http://imgur.com/lT0YmUf","http://imgur.com/kRguMFQ","http://imgur.com/tLhPnvd","http://imgur.com/XAakAYy","http://imgur.com/Sjr9pCD"];
+var eye=["http://imgur.com/lT0YmUf.png","http://imgur.com/kRguMFQ.png","http://imgur.com/tLhPnvd.png","http://imgur.com/XAakAYy.png","http://imgur.com/Sjr9pCD.png"];
 var nose=["http://i.imgur.com/pMiFXmT.png","http://i.imgur.com/DDXRQhR.png","http://i.imgur.com/5qw7JJH.png","http://i.imgur.com/PUvV9EB.png","http://i.imgur.com/SKX8DiK.png"];
 var mouth=["http://i.imgur.com/iO6sc30.png","http://i.imgur.com/9Qwo6fA.png","http://i.imgur.com/Oq5fJDJ.png","http://i.imgur.com/7L1x2it.png"];
 
