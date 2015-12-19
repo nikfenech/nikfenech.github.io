e8799715485a8108b914df088850c267762471c1
nikfenech <nik.fenech@gmail.com>
2015-12-20 04:49:11 +1100

    Isobar App

---
+++ isobar/js/scroll.js
@@ -0,0 +1,20 @@
+var scrollOne = function(){
+	TweenLite.to(window, 1, {scrollTo:{y:670}, ease:Power2.easeOut});
+}
+
+var scrollTwo = function(){
+	TweenLite.to(window, 1, {scrollTo:{y:1400}, ease:Power2.easeOut});
+}
+
+var scrollThree = function(){
+	TweenLite.to(window, 1, {scrollTo:{y:2100}, ease:Power2.easeOut});
+}
+
+var scrollFour = function(){
+	TweenLite.to(window, 1, {scrollTo:{y:2900}, ease:Power2.easeOut});
+}
+
+
+var scrollFive = function(){
+	TweenLite.to(window, 1, {scrollTo:{y:3500}, ease:Power2.easeOut});
+}
